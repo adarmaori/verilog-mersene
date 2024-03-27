d818f01945a97f267e94591d385f8574acd2d7ce
Adar Maori <40212269+adarmaori@users.noreply.github.com>
2024-03-27 16:12:10 +0200

    added shifting before subtraction

--- divisibility_checker.sv
+++ divisibility_checker.sv
@@ -27,33 +27,55 @@ module divisibility_checker (
   reg sub_a = 0;
   reg sub_b = 0;
 
+  reg [31:0] shifted = 0;
   always @ (posedge clk) begin
     case (state)
       S_COMP: begin
         comp_bit_divident = divident[NUMBER_LENGTH-index-1]; // This will eventually be a memory fetch
-        comp_bit_divisor = divisor[NUMBER_LENGTH-index-1]; // This will eventually be a memory fetch
+        comp_bit_divisor = divisor[NUMBER_LENGTH-index-1-shifted]; // This will eventually be a memory fetch
         comp_result = {comp_bit_divisor == comp_bit_divident, comp_bit_divident > comp_bit_divisor};
         case (comp_result)
-          2'b01: begin 
-            $display("Switching to SUB mode");
+          2'b00: begin
+            // divisor is larger than divident
+            // if it's shifted, shift it back once and subtract
+            // else, go to end condition
+            if (shifted) begin
+              shifted <= shifted - 1;
+              $display("Switching to SUB mode");
+              state = S_SUB;
+              index = 0;
+            end else begin
+              $display("Switching to END mode");
+              state = S_END;
+              index = 0;
+            end
+          end
+          2'b10: begin
+            // divisor and divident are equal up to that bit
+            index = index + 1;
+          end
+          default: begin 
+            // divident is still larger than divisor.
+            // if the first bits are different, shift again
+            // else, go to sub
+            if (index) begin
+              $display("Switching to SUB mode");
+              state = S_SUB;
+            end else begin
+              shifted = shifted + 1;
+            end
             index = 0;
-            state = S_SUB;
-          end
-          2'b00: begin 
-            $display("Switching to END mode");
-            state = S_END;
-            index <= 0;
-          end
-          default : begin
-            index <= index + 1;
           end
         endcase
 
       end
       S_SUB: begin
         sub_a = divident[index];
-        sub_b = divisor[index];
-
+        if (index >= shifted) begin
+          sub_b = divisor[index-shifted];
+        end else begin
+          sub_b = 0;
+        end
         sub_res = (sub_a^sub_b)^borrow;
         borrow = (~sub_a)&sub_b | borrow&(~(sub_a^sub_b));
         divident[index] = sub_res;
@@ -61,6 +83,7 @@ module divisibility_checker (
           state = S_COMP;
           $display("Switching to COMP mode");
           index = 0;
+          shifted = 0;
         end else begin
           index = index + 1;
         end
